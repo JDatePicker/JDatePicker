a47f5bf59e126486d11d5961219f23b8de14f087
jcoosthuizen <jc.oosthuizen@gmail.com>
2008-11-19 09:43:02 +0000

    Added a next and previous year button. Added setters and getter for the tooltips for the buttons and added default values for the tooltips.

--- src/main/java/net/sourceforge/jdatepicker/JPreviousIcon.java
+++ src/main/java/net/sourceforge/jdatepicker/JPreviousIcon.java
@@ -12,8 +12,23 @@ public class JPreviousIcon implements Icon {
 	protected int[] xPoints = new int[3];
 	protected int[] yPoints = new int[3];
 
+	private boolean doubleArrow = false;
+
 	public JPreviousIcon(int width, int height) {
 		setDimension(width, height);
+	}
+
+	public JPreviousIcon(int width, int height, boolean doubleArrow) {
+		setDimension(width, height);
+		this.doubleArrow = doubleArrow;
+	}
+
+	public boolean getDoubleArrow() {
+		return doubleArrow;
+	}
+
+	public void setDoubleArrow(boolean doubleArrow) {
+		this.doubleArrow = doubleArrow;
 	}
 
 	public int getIconWidth() {
@@ -30,16 +45,43 @@ public class JPreviousIcon implements Icon {
 	}
 
 	public void paintIcon(Component c, Graphics g, int x, int y) {
-		xPoints[0] = x;
-		yPoints[0] = y + (height / 2);
-		
-		xPoints[1] = x + width;
-		yPoints[1] = y - 1;
-		
-		xPoints[2] = x + width;
-		yPoints[2] = y + height;
-		
-		g.setColor(Color.BLACK);
-		g.fillPolygon(xPoints, yPoints, 3);
+		if (doubleArrow) {
+			xPoints[0] = x;
+			yPoints[0] = y + (height / 2);
+			
+			xPoints[1] = x + (width / 2);
+			yPoints[1] = y - 1;
+			
+			xPoints[2] = x + (width / 2);
+			yPoints[2] = y + height;
+			
+			g.setColor(Color.BLACK);
+			g.fillPolygon(xPoints, yPoints, 3);
+
+			xPoints[0] = x + (width / 2);
+			yPoints[0] = y + (height / 2);
+			
+			xPoints[1] = x + width;
+			yPoints[1] = y - 1;
+			
+			xPoints[2] = x + width;
+			yPoints[2] = y + height;
+			
+			g.setColor(Color.BLACK);
+			g.fillPolygon(xPoints, yPoints, 3);
+		} else {
+			xPoints[0] = x;
+			yPoints[0] = y + (height / 2);
+			
+			xPoints[1] = x + width;
+			yPoints[1] = y - 1;
+			
+			xPoints[2] = x + width;
+			yPoints[2] = y + height;
+			
+			g.setColor(Color.BLACK);
+			g.fillPolygon(xPoints, yPoints, 3);
+		}
 	}
 }
+
