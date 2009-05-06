255b7e01552a8d6212ea71689b54ff0847d89fc0
huangyue14603 <u4214110@anu.edu.au>
2009-05-06 04:25:07 +0000

    add function for event notification in JDatePicker.java

---
+++ src/main/java/net/sourceforge/jdatepicker/EventNotification.java
@@ -0,0 +1,414 @@
+package net.sourceforge.jdatepicker;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+import java.io.*;
+import java.util.*;
+import java.lang.String; 
+import javax.swing.table.AbstractTableModel;
+
+public class EventNotification extends JPanel{
+  public Vector<String> events = new Vector<String>();
+  private JButton setEvent = new JButton("Set Enevt");
+  private JButton viewEvent = new JButton("View Enevt");
+
+  private long nextID = 0;
+  private JLabel id = new JLabel("Event ID: "+nextID);
+  private JDialog setEventDia;
+  private JTextArea ta;
+  private JLabel JLdate;
+  private String curDate = "";
+  private JLabel checkDate;
+  private JLabel Jtype = new JLabel("Notification Type: ");
+  private JCheckBox tcb = new JCheckBox("Pop Text Window");
+  private JCheckBox acb = new JCheckBox("Audio");
+  private JLabel jdescription = new JLabel("Set Description For Your Envent:");
+
+  private JButton addEvent = new JButton("New");
+  private JButton delEvent = new JButton("Delete");
+  private JButton editEvent = new JButton("Edit");
+  private JButton applyEvent = new JButton("Apply");
+  private JButton setEventOK = new JButton("OK");
+  private JButton setEventCancel = new JButton("Cancel");
+  private MyTableModel tm;
+  private JTable table;
+  private JScrollPane jp;
+  private JPanel tp;
+
+  private JDialog viewEventDia;
+  private JButton viewOK = new JButton("OK");
+
+  private int selectedRow=0;
+  private Boolean edit = false;
+
+  public EventNotification(){
+    super();
+    initialize();
+  }
+
+  private void initialize(){
+    this.setLayout(new GridLayout(3,1));
+    JLabel space = new JLabel();
+    this.add(setEvent);
+    this.add(space);
+    this.add(viewEvent);
+    addEventListener aeh = new addEventListener();
+    addEvent.addActionListener(aeh);
+    applyListener ah = new applyListener();
+    applyEvent.addActionListener(ah);
+    setEventListener seh = new setEventListener();
+    setEvent.addActionListener(seh);
+    viewEventListener veh = new viewEventListener();
+    viewEvent.addActionListener(veh);
+    setEventOKListener seokh = new setEventOKListener();
+    setEventOK.addActionListener(seokh);
+    setEventCancelListener secancelh = new setEventCancelListener();
+    setEventCancel.addActionListener(secancelh);
+    delEventListener dh = new delEventListener();
+    delEvent.addActionListener(dh);
+    editEventListener eh = new editEventListener();
+    editEvent.addActionListener(eh);
+    vOKEventListener vokh = new vOKEventListener();
+    viewOK.addActionListener(vokh);
+    loadEvents();
+  }
+  
+  public void setNotificationDate(String date){
+    JLdate = new JLabel();
+    JLdate.setText("Notification Date: "+date);
+    JLdate.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 14));
+  }
+
+  public void outjdp()throws IOException{
+    OutputStreamWriter output = null; //create a writer for out a config file
+    File file = new File("net/sourceforge/jdatepicker/events/events.jdp");
+    try { 
+      output = new OutputStreamWriter(new BufferedOutputStream(new FileOutputStream(file))); //create a writer for out config file
+    }catch(FileNotFoundException e) {
+      System.err.println("Cannot open events.jdp for output");
+    } 
+    try{
+      String str="";
+      for(int i=0; i<events.size(); i++){
+        str+=events.get(i)+"\n";
+      }
+      output.write(str,0,str.length());//write all infos to a jdp file
+    }catch(IOException e){
+    }
+    output.close();
+  }
+
+  public void loadEvents(){
+    File file = new File("net/sourceforge/jdatepicker/events/events.jdp");
+    try{
+      BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file))); 
+      try{
+        String str_br=br.readLine(); //try to read first line
+        if(str_br==null){
+          nextID=0;
+        }
+        while(str_br!=null){
+          events.add(str_br);
+          str_br=br.readLine(); //read next line
+        }
+      }catch(IOException e){
+      }
+    }catch(FileNotFoundException e){
+      System.err.println ("File events.jdp can't be found");
+    }
+  }
+
+  public void addTP(JPanel jtp){
+    jtp.add(jp);
+  }
+
+  /**listener for addEvent **/
+  private class addEventListener implements ActionListener {
+    public void actionPerformed(ActionEvent event){
+      edit = false;
+      acb.setSelected(false);
+      JLdate.setText(curDate);
+      ta.setText("");
+      if(events.size()>0){
+        StringTokenizer  st = new StringTokenizer(events.get(events.size()-1));
+        nextID = Integer.parseInt(st.nextToken())+1;
+      }else{
+        nextID = nextID+1;
+      }
+      id.setText("Event ID: "+nextID);
+    }
+  }
+
+  /**listener for editEvent **/
+  private class editEventListener implements ActionListener {
+    public void actionPerformed(ActionEvent event){
+      edit = true;
+      selectedRow = table.getSelectedRow();
+      for(int i=0; i<4; i++){
+        if(i==0){ //get id
+          id.setText("Event ID: "+tm.getValueAt(table.getSelectedRow(),i));
+        }else if (i==1){ //get date
+          JLdate.setText("Notification Date: "+tm.getValueAt(table.getSelectedRow(),i));
+        }else if (i==2){ //get alarm
+          if(((String)tm.getValueAt(table.getSelectedRow(),i)).compareTo("Yes")==0){
+            acb.setSelected(true);
+          }else{
+            acb.setSelected(false);
+          }
+        }else{ //get description
+          ta.setText((String)tm.getValueAt(table.getSelectedRow(),i));
+        }
+      }
+    }
+  }
+
+  /**listener for setEvent **/
+  private class setEventListener implements ActionListener {
+    public void actionPerformed(ActionEvent event){
+      edit = false;
+      setEventDialog();
+    }
+  }
+
+  /**listener for delEvent **/
+  private class delEventListener implements ActionListener {
+    public void actionPerformed(ActionEvent event){
+      edit = false;
+      for(int i=table.getSelectedRow()+table.getSelectedRowCount()-1; i>=table.getSelectedRow();i--){
+        events.remove(i);
+      }
+      try{
+        outjdp();
+      }catch(IOException e) {
+        e.printStackTrace();
+      }
+      setEventDia.dispose();
+      setEventDialog();
+    }
+  }
+
+  /**listener for apply **/
+  private class applyListener implements ActionListener {
+    public void actionPerformed(ActionEvent event){
+      String str = "";
+      str = id.getText().substring(10, id.getText().length()) + " " + JLdate.getText().substring(19,JLdate.getText().length())+" ";
+      if(acb.isSelected()==true){
+        str += "1 ";
+      }else{
+        str += "0 ";
+      }
+      str +=ta.getText();
+      if(edit == false){
+        events.add(str);
+        acb.setSelected(false);
+        ta.setText("");
+      }else{
+        events.set(selectedRow,str);
+      }
+      try{
+        outjdp();
+      }catch(IOException e) {
+        e.printStackTrace();
+      }
+      setEventDia.dispose();
+      setEventDialog();
+    }
+  }
+
+  /**listener for viewEvent **/
+  private class viewEventListener implements ActionListener {
+    public void actionPerformed(ActionEvent event){
+      viewEventDialog();
+    }
+  }
+
+  /**listener for viewOK**/
+  private class vOKEventListener implements ActionListener {
+    public void actionPerformed(ActionEvent event){
+      viewEventDia.dispose();
+    }
+  }
+
+  /**listener for setEventOK **/
+  private class setEventOKListener implements ActionListener {
+    public void actionPerformed(ActionEvent event){
+      String str = "";
+      str = id.getText().substring(10, id.getText().length()) + " " + JLdate.getText().substring(19,JLdate.getText().length())+" ";
+      if(acb.isSelected()==true){
+        str += "1 ";
+      }else{
+        str += "0 ";
+      }
+      str +=ta.getText();
+      if(edit == false){
+        events.add(str);
+      }else{
+        events.set(selectedRow,str);
+      }
+      try{
+        outjdp();
+      }catch(IOException e) {
+        e.printStackTrace();
+      }
+      edit = false;
+      setEventDia.dispose();
+    }
+  }
+
+  /**listener for setEventCancel **/
+  private class setEventCancelListener implements ActionListener {
+    public void actionPerformed(ActionEvent event){
+      edit = false;
+      setEventDia.dispose();
+    }
+  }
+  
+  private class MyTableModel extends AbstractTableModel {
+    private String[] columnNames = {"ID", "Date", "Audio Alarm", "Description"};
+    private Object[][] data = new Object[events.size()][4];
+    public int getColumnCount() {
+      return columnNames.length;
+    }
+
+    public int getRowCount() {
+      return data.length;
+    }
+
+    public String getColumnName(int col) {
+      return columnNames[col];
+    }
+
+    public Object getValueAt(int row, int col) {
+      return data[row][col];
+    }
+
+    public Class getColumnClass(int c) {
+      return getValueAt(0, c).getClass();
+    }
+
+    public void setValueAt(Object value, int row, int col) {
+      data[row][col] = value;
+      fireTableCellUpdated(row, col);
+    }
+  }
+  
+  public void addTable(){
+    tm = new MyTableModel();
+    table = new JTable(tm);
+    table.setAutoCreateRowSorter(true);
+    for(int i=0;i<events.size();i++){
+      Vector<String> rd = new Vector<String>();
+      StringTokenizer  st = new StringTokenizer(events.get(i)); //need identify
+      int j=0;
+      String str = "";
+      while(st.hasMoreTokens()){
+        if(j==0){
+          rd.add(st.nextToken());
+        }else if(j>=1 && j<=3){
+          if(j==3){
+            str += st.nextToken();
+            rd.add(str);
+            str="";
+          }else{
+            str += st.nextToken()+" ";
+          }
+        }else if(j==4){
+          if(st.nextToken().compareTo("1")==0){
+            rd.add("Yes");
+          }else{
+            rd.add("No");
+          }
+        }else if(j>=5){
+          str+=st.nextToken()+" ";
+        }
+        j++;
+      }
+      rd.add(str);
+      for(j=0; j<rd.size();j++){
+        tm.setValueAt((Object)rd.get(j),i,j);
+      }
+    }
+    jp = new JScrollPane(table);
+  }
+
+  /**method for set event dialog **/
+  public void setEventDialog(){
+    setEventDia = new JDialog(null,"Set Event",Dialog.ModalityType.APPLICATION_MODAL);
+    if(events.size()>0){
+      StringTokenizer  st = new StringTokenizer(events.get(events.size()-1));
+      nextID = Integer.parseInt(st.nextToken())+1;
+    }else{
+      nextID = 0;
+    }
+    id.setText("Event ID: "+nextID);
+    edit = false;
+    addTable();
+
+    curDate = JLdate.getText();
+    Container descriptionC = new Container();
+    descriptionC.setLayout(new BorderLayout());
+    jdescription.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 14));
+    descriptionC.add(jdescription, BorderLayout.NORTH);
+    ta = new JTextArea ();
+    ta.setLineWrap(true); 
+    JScrollPane jsp = new JScrollPane(ta);
+    descriptionC.add(jsp,BorderLayout.CENTER);
+    tp = new JPanel();
+    tp.setLayout(new GridLayout(2,1));
+    tp.add(descriptionC);
+    tp.add(jp);
+
+    Container ttc = new Container();
+    ttc.setLayout(new GridLayout(3,1));
+    ttc.add(id);
+    id.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 14));
+    ttc.add(JLdate);
+    ttc.add(Jtype);
+    Jtype.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 14));
+    Container tb = new Container();
+    tb.setLayout(new GridLayout(1,2));
+    tb.add(tcb);
+    tb.add(acb);
+    tcb.setSelected(true);
+    tcb.setEnabled(false);
+    acb.setSelected(false);
+    Container tc = new Container();
+    tc.setLayout(new BorderLayout());
+    tc.add(ttc, BorderLayout.NORTH);
+    tc.add(tb, BorderLayout.CENTER);
+
+    Container setButtons = new Container();
+    setButtons.setLayout(new GridLayout(2,3));
+    setButtons.add(addEvent);
+    setButtons.add(editEvent);
+    setButtons.add(delEvent);
+    setButtons.add(setEventOK);
+    setButtons.add(applyEvent);
+    setButtons.add(setEventCancel);
+    setEventDia.setLayout(new BorderLayout());
+    setEventDia.add(tc,BorderLayout.NORTH);
+    setEventDia.add(tp,BorderLayout.CENTER);
+    setEventDia.add(setButtons,BorderLayout.SOUTH);
+    setEventDia.setSize(500,500); //set dialog size
+    setEventDia.setLocation(this.getGraphicsConfiguration().getBounds().width/8,
+                        this.getGraphicsConfiguration().getBounds().height/8); //set location of dialog diaplay
+    setEventDia.setAlwaysOnTop(true); //set dialog always on top of any windows
+    setEventDia.setVisible(true); //set visible of dialog of timer seting
+  }
+
+ /**method for view event dialog **/
+  public void viewEventDialog(){
+    viewEventDia = new JDialog(null,"View Event",Dialog.ModalityType.APPLICATION_MODAL);
+    addTable();
+    viewEventDia.setLayout(new BorderLayout());
+    viewEventDia.add(jp,BorderLayout.CENTER);
+    viewEventDia.add(viewOK,BorderLayout.SOUTH);
+    viewEventDia.setSize(400,400); //set dialog size
+    viewEventDia.setLocation(this.getGraphicsConfiguration().getBounds().width/8,
+                        this.getGraphicsConfiguration().getBounds().height/8); //set location of dialog diaplay
+    viewEventDia.setAlwaysOnTop(true); //set dialog always on top of any windows
+    viewEventDia.setVisible(true); //set visible of dialog of timer seting
+  }
+}
--- src/main/java/net/sourceforge/jdatepicker/JDatePicker.java
+++ src/main/java/net/sourceforge/jdatepicker/JDatePicker.java
@@ -1,9 +1,12 @@
 package net.sourceforge.jdatepicker;
+
+import java.awt.*;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.BorderLayout;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,7 +15,11 @@ import java.awt.event.HierarchyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.Calendar;
+import java.util.Date;
+import java.text.DateFormat;
+import java.util.GregorianCalendar;
 
+import javax.swing.*;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -23,6 +30,11 @@ import javax.swing.PopupFactory;
 import javax.swing.UIManager;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import java.util.Timer;
+import java.util.*;
+import java.lang.String;
+import javax.sound.midi.*;
+import java.io.*;
 
 
 /**
@@ -48,10 +60,18 @@ public class JDatePicker extends JPanel {
 				System.exit(0);
 			}
 		});
-		testFrame.setSize(600,300);
+		testFrame.setSize(250,350);
 		JPanel jPanel = new JPanel();
+    JEventNotification();
 		jPanel.add(new JDatePicker());
-		testFrame.getContentPane().add(jPanel);
+    JPanel DatePanel = new JPanel();
+    DatePanel.setLayout(new BorderLayout());
+    DatePanel.add(jPanel, BorderLayout.WEST);
+    DatePanel.add(eventNotification, BorderLayout.SOUTH);
+    BorderLayout fb = new BorderLayout();
+    testFrame.setLayout(fb);
+    testFrame.getContentPane().add(jmb, fb.NORTH);
+		testFrame.getContentPane().add(DatePanel, fb.WEST);
 		testFrame.setVisible(true);
 	}
 
@@ -60,7 +80,7 @@ public class JDatePicker extends JPanel {
 	private Popup popup;
 	private JTextField dateTextField;
 	private JButton editButton;
-	private JDatePanel datePanel;
+	private static JDatePanel datePanel;
 
 	/**
 	 * Constructs a JDatePicker
@@ -105,6 +125,7 @@ public class JDatePicker extends JPanel {
 		int w = (int)datePanel.getPreferredSize().getWidth();
 		editButton.setPreferredSize(new Dimension(h,h));
 		dateTextField.setPreferredSize(new Dimension(w-h+1,h));
+    eventNotification.setNotificationDate(datePanel.toString());
 
 		//Add event listeners
 		editButton.addActionListener(eventHandler);
@@ -184,7 +205,161 @@ public class JDatePicker extends JPanel {
 		public void stateChanged(ChangeEvent arg0) {
 			if (arg0.getSource()==datePanel)
 				dateTextField.setText(datePanel.toString());
+        eventNotification.setNotificationDate(datePanel.toString());
 		}
 	}
 
+  private static EventNotification eventNotification;
+  private static JPanel menuPanel= new JPanel();
+  private static JMenuBar jmb = new JMenuBar();
+  private static JMenu menuFile = new JMenu("File");
+  private static JMenuItem mSetEvent = new JMenuItem("Set Event");
+  private static JMenuItem mViewEvent = new JMenuItem("View Event");
+  private static JMenuItem mQuit = new JMenuItem ("Quit");
+  public static void JEventNotification(){
+    Timer t=new Timer();
+    TimerTask tt = new playTimer();
+    t.schedule(tt,5000,30000);
+    menuFile.add(mSetEvent);
+    menuFile.add(mViewEvent);
+    menuFile.add(mQuit);
+    jmb.add(menuFile);
+    eventNotification = new EventNotification();
+    MsetEventListener sehM = new MsetEventListener();
+    mSetEvent.addActionListener(sehM);
+    MviewEventListener vehM = new MviewEventListener();
+    mViewEvent.addActionListener(vehM);
+    MquitListener qhM = new MquitListener();
+    mQuit.addActionListener(qhM);
+  }
+
+  /**listener for mSetEvent **/
+  private static class MsetEventListener implements ActionListener {
+    public void actionPerformed(ActionEvent event){
+      eventNotification.setEventDialog();
+    }
+  }
+
+  /**listener for mViewEvent **/
+  private static class MviewEventListener implements ActionListener {
+    public void actionPerformed(ActionEvent event){
+      System.out.println("View");
+    }
+  }
+
+  /**listener for mQuit **/
+  private static class MquitListener implements ActionListener {
+    public void actionPerformed(ActionEvent event){
+      System.exit(1);
+    }
+  }
+
+  private static JLabel checkDate = new JLabel();
+  private static Calendar ct = new GregorianCalendar();
+  private static DateFormat df = DateFormat.getDateInstance(DateFormat.LONG);
+  /** class for timer to notify the event **/
+  private static class playTimer extends TimerTask{
+    @Override
+    public void run(){
+      checkDate.setText(ct.get(Calendar.DATE)+" "+converMonth(ct.get(Calendar.MONTH)+1)+" "+ct.get(Calendar.YEAR));
+      System.out.println(datePanel.toString());
+      Vector<Integer> v_idx = new Vector<Integer>();
+      Vector<String> v_description = new Vector<String>();
+      for(int i=0;i<eventNotification.events.size();i++){
+        Vector<String> v_info = new Vector<String>();
+        Vector<String> v_date = new Vector<String>();
+        stringToken(v_info, eventNotification.events.get(i));
+        stringToken(v_date, checkDate.getText());
+        if(v_info.get(1).compareTo(v_date.get(0))==0 && v_info.get(2).compareTo(v_date.get(1))==0 && v_info.get(3).compareTo(v_date.get(2))==0){
+          v_idx.add(i);
+        }
+      }
+      if(v_idx.isEmpty()==false){
+        String str = "";
+        for(int i=0; i<v_idx.size();i++){
+          stringToken(v_description, eventNotification.events.get(v_idx.get(i)));
+          for(int j=5;j<v_description.size();j++){
+            str+=v_description.get(j)+" ";
+          }
+          str += "\n\n";
+        }
+        for(int i=v_idx.size()-1; i>=0; i--){
+          eventNotification.events.remove((int)v_idx.get(i));
+        }
+        try{
+          eventNotification.outjdp();
+        }catch(IOException e) {
+          e.printStackTrace();
+        }
+        if(v_description.get(4).compareTo("1")==0){
+          openAlarm();
+        }
+        JOptionPane.showMessageDialog(null,str,"Event",JOptionPane.INFORMATION_MESSAGE);
+      }
+    }
+    public String converMonth(int m){
+      String str_m="";
+      if(m==1){
+        str_m = "January";
+      }else if(m==2){
+        str_m = "February";
+      }else if(m==3){
+        str_m = "March";
+      }else if(m==4){
+        str_m = "April";
+      }else if(m==5){
+        str_m = "May";
+      }else if(m==6){
+        str_m = "June";
+      }else if(m==7){
+        str_m = "July";
+      }else if(m==8){
+        str_m = "August";
+      }else if(m==9){
+        str_m = "September";
+      }else if(m==10){
+        str_m = "October";
+      }else if(m==11){
+        str_m = "November";
+      }else if(m==12){
+        str_m = "December";
+      }
+      return str_m;
+    }
+    /* method for get infomation of one line*/
+    public void stringToken(Vector<String> v, String t){
+      StringTokenizer  st = new StringTokenizer(t);
+      while(st.hasMoreTokens()){
+        v.add(st.nextToken());
+      }
+    }
+    public void openAlarm() {
+      Sequencer sequencer = null;
+      Sequence sequence = null;
+      Synthesizer synthesizer = null;
+      Instrument instruments[];
+      MidiChannel channels[];
+      MidiChannel cc = null;
+      try {
+        if (synthesizer == null) {
+          if ((synthesizer = MidiSystem.getSynthesizer()) == null) {
+            System.out.println("getSynthesizer() failed!");
+            return;
+          }
+        } 
+        synthesizer.open();
+        sequencer = MidiSystem.getSequencer();
+        sequence = new Sequence(Sequence.PPQ, 10);
+      } catch (Exception ex) { ex.printStackTrace(); return; }
+      Soundbank sb = synthesizer.getDefaultSoundbank();
+    
+      if (sb != null) {
+        instruments = synthesizer.getDefaultSoundbank().getInstruments();
+        synthesizer.loadInstrument(instruments[0]);
+      }
+      MidiChannel midiChannels[] = synthesizer.getChannels();
+      cc = midiChannels[13]; 
+      cc.noteOn(86, 80);
+    }
+  }
 }
