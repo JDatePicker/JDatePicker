e141ac19be881c1eae3e9c6c28e06d83d8a7acf0
juanheyns <juanheyns@b64d7f05-614e-4fbc-889b-a5d368fbb5c2>
2010-04-27 06:56:33 +0000

    Creating tag for 1.3.1

---
+++ tags/1.3.1/src/main/java/net/sourceforge/jdatepicker/impl/SqlDateModel.java
@@ -0,0 +1,58 @@
+/**
+Copyright 2004 Juan Heyns. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are
+permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice, this list of
+      conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice, this list
+      of conditions and the following disclaimer in the documentation and/or other materials
+      provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY JUAN HEYNS ``AS IS'' AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL JUAN HEYNS OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those of the
+authors and should not be interpreted as representing official policies, either expressed
+or implied, of Juan Heyns.
+*/
+package net.sourceforge.jdatepicker.impl;
+
+import java.sql.Date;
+import java.util.Calendar;
+
+import net.sourceforge.jdatepicker.AbstractDateModel;
+
+public class SqlDateModel extends AbstractDateModel<java.sql.Date> {
+
+	public SqlDateModel() {
+		this(null);
+	}
+	
+	public SqlDateModel(Date value) {
+		super();
+		setValue(value);
+	}
+
+	@Override
+	protected Date fromCalendar(Calendar from) {
+		return new Date(from.getTimeInMillis());
+	}
+
+	@Override
+	protected Calendar toCalendar(Date from) {
+		Calendar to = Calendar.getInstance();
+		to.setTime(from);
+		return to;
+	}
+
+}
