1fa18dc3ad28a872ec8ea0b729a26a63fb1ac353
Juan Heyns <juanheyns@gmail.com>
2015-10-13 12:42:03 +0200

    Fixes issue #58.

--- src/main/java/org/jdatepicker/JDatePanel.java
+++ src/main/java/org/jdatepicker/JDatePanel.java
@@ -729,6 +729,11 @@ public class JDatePanel extends JComponent implements DatePanel {
         private static final long serialVersionUID = -2341614459632756921L;
 
         public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+            // Exit this method if the value is null, encountered from JTable#AccessibleJTable
+            if (value == null) {
+                return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+            }
+
             JLabel label = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
             label.setHorizontalAlignment(JLabel.CENTER);
 
